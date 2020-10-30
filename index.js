b10d7f020cce520f137e63c936152d67123ab518
manhnvan <manh12344444444@gmail.com>
2020-10-30 22:50:54 +0700

    generate project

---
+++ index.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+// connect mongoDB
+mongoose.connect(process.env.URI, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useCreateIndex: true
+});
+
+// express config
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+// middlware place
+
+// import router
+
+// config third party moudules
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With', 'Content-Type', 'Accept', 'Authorization');
+	if (req.method === 'OPTIONS') {
+		req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+	next();
+});
+
+// Router config place
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
